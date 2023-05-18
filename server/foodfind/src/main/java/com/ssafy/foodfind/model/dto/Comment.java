bdc967da11e0543544688dcf2e5461c99798fd9a
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-05-18 17:43:10 +0900

    feat : Comment dto 추가

---
+++ server/foodfind/src/main/java/com/ssafy/foodfind/model/dto/Comment.java
@@ -0,0 +1,60 @@
+package com.ssafy.foodfind.model.dto;
+
+public class Comment {
+	private int commentId;
+	private int userId;
+	private int truckId;
+	private int rating;
+	private String content;
+	
+	public Comment() {
+		super();
+	}
+	
+	public Comment(int commentId, int userId, int truckId, int rating, String content) {
+		super();
+		this.commentId = commentId;
+		this.userId = userId;
+		this.truckId = truckId;
+		this.rating = rating;
+		this.content = content;
+	}
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getTruckId() {
+		return truckId;
+	}
+	public void setTruckId(int truckId) {
+		this.truckId = truckId;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [commentId=" + commentId + ", userId=" + userId + ", truckId=" + truckId + ", rating=" + rating
+				+ ", content=" + content + "]";
+	}
+	
+	
+}
