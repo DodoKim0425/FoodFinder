bbef94dc82fcd5f9c9d0e25c90a9af1ef4b61e65
noion0511 <noion0511@gmail.com>
2023-05-20 14:26:59 +0900

    feat : truck 조회 기능

--- android/app/src/main/java/com/ssafy/foodfind/ui/managetruck/ManageTruckItemActivity.kt
+++ android/app/src/main/java/com/ssafy/foodfind/ui/managetruck/ManageTruckItemActivity.kt
@@ -3,21 +3,40 @@ package com.ssafy.foodfind.ui.managetruck
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.ssafy.foodfind.R
+import com.ssafy.foodfind.SharedPrefs
 import com.ssafy.foodfind.databinding.ActivityCustomerOrderListBinding
+import com.ssafy.foodfind.databinding.ActivityLoginBinding
 import com.ssafy.foodfind.databinding.ActivityManageTruckItemBinding
+import com.ssafy.foodfind.ui.LoadingDialog
+import com.ssafy.foodfind.ui.base.BaseActivity
+import com.ssafy.foodfind.ui.home.MainActivity
+import com.ssafy.foodfind.ui.login.LoginViewModel
 import com.ssafy.foodfind.ui.shoppingcart.ShoppingCartActivity
 import dagger.hilt.android.AndroidEntryPoint
 
+private const val TAG = "ManageTruckItemActivity"
+
 @AndroidEntryPoint
-class ManageTruckItemActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityManageTruckItemBinding
+class ManageTruckItemActivity :
+    BaseActivity<ActivityManageTruckItemBinding>(R.layout.activity_manage_truck_item) {
+
+    private val viewModel by viewModels<TruckViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityManageTruckItemBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         initButton()
+        initTruck()
+        observeData()
+    }
+
+    private fun initTruck() {
+        if (SharedPrefs.getUserInfo() != null) {
+            val userId = SharedPrefs.getUserInfo()!!.userId
+            viewModel.getMyTruckInfo(userId)
+        }
     }
 
     private fun initButton() {
@@ -28,6 +47,40 @@ class ManageTruckItemActivity : AppCompatActivity() {
         binding.btnUpdateTruck.setOnClickListener {
             val intent = Intent(this, ManageTruckActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun observeData() {
+        with(viewModel) {
+            errorMsg.observe(this@ManageTruckItemActivity) { event ->
+                event.getContentIfNotHandled()?.let {
+                    showToast(it)
+                }
+            }
+
+            val dialog = LoadingDialog(this@ManageTruckItemActivity)
+            isLoading.observe(this@ManageTruckItemActivity) {
+                if (isLoading.value!!) {
+                    dialog.show()
+                } else if (!isLoading.value!!) {
+                    dialog.dismiss()
+                }
+            }
+
+            truck.observe(this@ManageTruckItemActivity) { event ->
+                event.getContentIfNotHandled()?.let { truck ->
+                    Log.d(TAG, "observeData: ")
+                    if (truck.truckId == 0) {
+                        //다이얼로그 띄우기
+                        val intent =
+                            Intent(this@ManageTruckItemActivity, ManageTruckActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.d(TAG, "observeData: ")
+                        binding.truck = truck
+                    }
+                }
+            }
         }
     }
 }
