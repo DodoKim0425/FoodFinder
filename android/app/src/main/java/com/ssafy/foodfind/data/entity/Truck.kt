e2c0eacf60dd6628583ab911c831985ec47cfcea
noion0511 <noion0511@gmail.com>
2023-05-20 13:55:01 +0900

    feat : truck entity에 필요한 데이터 추가

--- android/app/src/main/java/com/ssafy/foodfind/data/entity/Truck.kt
+++ android/app/src/main/java/com/ssafy/foodfind/data/entity/Truck.kt
@@ -3,8 +3,11 @@ package com.ssafy.foodfind.data.entity
 data class Food(val name: String, val price: String)
 
 data class Truck(
-    val name: String,
-    val rating: Float,
-    val description: String,
-    val location: String
+    val truckId: Int = 0,
+    val ownerId: Int = 0,
+    val name: String = "",
+    val rating: Float = 0.0F,
+    val description: String = "",
+    val location: String = "",
+    val currentStatus: String = ""
 )
