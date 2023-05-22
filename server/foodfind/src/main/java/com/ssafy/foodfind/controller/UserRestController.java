5630d6b8685f9955ee4d3725d01e0c93357aa45c
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-05-22 10:22:31 +0900

    refact : user 정보 수정을 PostMapping에서 PutMapping으로 변경

--- server/foodfind/src/main/java/com/ssafy/foodfind/controller/UserRestController.java
+++ server/foodfind/src/main/java/com/ssafy/foodfind/controller/UserRestController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +62,7 @@ public class UserRestController {
 		return isExist;
 	}
 	
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public boolean update(@RequestBody User user) {
 		return uService.update(user);
 	}
