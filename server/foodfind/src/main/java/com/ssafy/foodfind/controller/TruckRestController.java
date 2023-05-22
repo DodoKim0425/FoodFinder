eee3fad76f5046184e55a477b02b1cf14cf0b0f1
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-05-22 10:18:29 +0900

    refact : 트럭 수정 api에서 PostMapping을 PutMapping으로 변경

--- server/foodfind/src/main/java/com/ssafy/foodfind/controller/TruckRestController.java
+++ server/foodfind/src/main/java/com/ssafy/foodfind/controller/TruckRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +55,7 @@ public class TruckRestController {
 		
 	}
 	
-	@PostMapping("/update")
+	@PutMapping("/update")
 	@ApiOperation(value="Truck 수정")
 	public Boolean update(@RequestBody Truck truck) {
 		tService.update(truck);
